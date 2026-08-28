fe50c18abe63234a22f4e55001042fdd5dd1256e
jakpotato <jack.dev.cc@gmail.com>
2026-08-28 15:42:09 -0600

    Update app.js

--- Clowder/app.js
+++ Clowder/app.js
@@ -130,7 +130,7 @@ function makePost() {
     id: id("post"),
     personId: person.id,
     text,
-    "Post",
+    category,
     createdAt: Date.now() - Math.floor(Math.random() * 1000 * 60 * 60 * 72),
     reactions: {
       like: Math.floor(Math.random() * 70),
