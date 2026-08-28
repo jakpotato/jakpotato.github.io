b7a190d2b243f1c1244443f1564c180f361fa1c3
jakpotato <jack.dev.cc@gmail.com>
2026-08-27 23:58:21 -0600

    Update content.js

--- Clowder/data/content.js
+++ Clowder/data/content.js
@@ -3,7 +3,7 @@ export const SUBJECTS = [
   "wet socks", "airport carpets", "cold fries", "parking lots", "socks", "the weather",
   "homework", "alarm clocks", "elevators", "receipts", "small talk", "mystery buttons",
   "the number 7", "plastic chairs", "cereal", "shopping carts", "charging cables",
-  "printer paper", "neighbours", "leftovers", "waiting rooms", "pens that barely work", "printers", "sharks", "guy from the zoo", "spicy books", "grandma", "bit of butter with crumbs on it", "the great Flying Spaghetti Monster", "noodles", "butterflies", "dinosaurs", "microwaves", "putting animals outside", "stars", "new spoons"
+  "printer paper", "neighbours", "leftovers", "waiting rooms", "pens that barely work", "printers", "sharks", "guy from the zoo", "spicy books", "grandma", "bit of butter with crumbs on it", "the great Flying Spaghetti Monster", "noodles", "butterflies", "dinosaurs", "microwaves", "putting animals outside", "glow in the dark stars", "new spoons"
 ];
 
 export const TEMPLATES = {
